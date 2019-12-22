9fa5b81efdc14d0386f2a39ee76b13c655c8f6eb
Kirill Novik <kino6052@colorado.edu>
2019-12-22 16:02:12 -0700

    Add Failing Tests

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,62 @@
+describe('...', () => {
+  it('should create an item', () => {
+    expect.hasAssertions()
+  })
+  it('should update an item', () => {
+    expect.hasAssertions()
+  })
+  it('should delete an item', () => {
+    expect.hasAssertions()
+  })
+  it('should get an item by id', () => {
+    expect.hasAssertions()
+  })
+  it('should get an item by name (partial)', () => {
+    expect.hasAssertions()
+  })
+  it('should rename an item', () => {
+    expect.hasAssertions()
+  })
+  it('should change place in a list', () => {
+    expect.hasAssertions()
+  })
+  it('should change parent', () => {
+    expect.hasAssertions()
+  })
+  it('should add comment', () => {
+    expect.hasAssertions()
+  })
+  it('should save everything', () => {
+    expect.hasAssertions()
+  })
+})
+
+describe('...', () => {
+  it('should create a note', () => {
+    expect.hasAssertions()
+  })
+  it('should update a note', () => {
+    expect.hasAssertions()
+  })
+  it('should delete a note', () => {
+    expect.hasAssertions()
+  })
+  it('should get a note by id', () => {
+    expect.hasAssertions()
+  })
+  it('should get a note by name (partial)', () => {
+    expect.hasAssertions()
+  })
+  it('should rename an note', () => {
+    expect.hasAssertions()
+  })
+  it('should add item parent', () => {
+    expect.hasAssertions()
+  })
+  it('should remove item parent', () => {
+    expect.hasAssertions()
+  })
+  it('should save everything', () => {
+    expect.hasAssertions()
+  })
+})
