7ad545b56a73f7f723c87b5844528dada4c9527c
Kirill Novik <kino6052@colorado.edu>
2020-01-01 15:55:24 -0700

    Make Test Pass

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,14 @@
 describe('...', () => {
   it('should add an item', () => {
+    interface Item {
+      title: string
+    }
+    const item: Item = {
+      title: 'Title',
+    }
+    const items: Item[] = []
+    const addItem = (item: Item, items: Item[]) => [...items, item]
+    const getTitles = (items: Item[]) => items.map(i => i.title)
     expect(getTitles(addItem(item, items))).toEqual(['Title'])
     expect(getTitles(addItem(item, addItem(item, items)))).toEqual([
       'Title',
